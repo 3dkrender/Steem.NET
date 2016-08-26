73d17d8a67b3b25a3f56ba6c02112d35ef1b7e0b
VIM-Arcange <vincent.mascart@gmail.com>
2016-08-26 15:34:27 +0200

    Simple example on how to use CSteemAPI

---
+++ Test.vb
@@ -0,0 +1,40 @@
+﻿Imports Newtonsoft.Json
+Module Test
+
+    Public Sub Main()
+
+        Using oSteem As New CSteemAPI
+
+            Debug.Print(oSteem.get_config.ToString)
+            Debug.Print(oSteem.get_account_count.ToString)
+
+            Dim arrAcct As New ArrayList
+            arrAcct.Add("arcange")
+            For Each oAccount In oSteem.get_accounts(arrAcct)
+                Debug.Print(oAccount.ToString)
+            Next
+
+            Dim oBlock As Linq.JObject
+            Dim oTrans As Linq.JObject
+
+            Dim lLastBlockID As Long = oSteem.get_dynamic_global_properties().Item("last_irreversible_block_num")
+
+            For lBlockID As Long = 4000000 To lLastBlockID
+                oBlock = oSteem.get_block(lBlockID)
+                If Not oBlock.Item("transactions") Is Nothing Then
+
+                    Debug.Print(String.Format("Block {0} {1} contains {2} transactions", oBlock.Item("block_num"),
+                                                                                         oBlock.Item("timestamp").ToObject(Of DateTime),
+                                                                                         oBlock.Item("transactions").Count))
+
+                    For Each oTrans In oBlock.Item("transactions")
+                        Debug.Print(String.Format("Transaction is {0} operations", oTrans.Item("operations").Item(0).Item(0)))
+                    Next
+                End If
+            Next
+
+        End Using
+
+    End Sub
+
+End Module
